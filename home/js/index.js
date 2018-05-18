7e554ab7ca5521a69ab46b634a3dd91eca8ed015
akong0115 <1099484683@qq.com>
2018-05-18 18:03:10 +0800

    终于发现不能居中是因为ie8不支持h5

--- home/js/index.js
+++ home/js/index.js
@@ -1,41 +1,78 @@
+'use strict';
+
 window.onload = function (e) {
+    // 兼容ie10以下对requestAnimation
+    window.requestAnimationFrame = window.requestAnimationFrame || function (fn) { return setTimeout(fn, 1000 / 60) }
+    window.cancelAnimationFrame = window.cancelAnimationFrame || clearTimeout;
+    //兼容bind函数
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function () {
+            if (typeof this !== 'function') {　　　　　　
+                throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');　　　　
+            }
+            var _this = this;
+            var obj = arguments[0];
+            var ags = Array.prototype.slice.call(arguments, 1);
+            return function () {
+                _this.apply(obj, ags);
+            };
+        };
+    }
+
+    //兼容addEventListener函数
+    function addEventListener(ele, event, fn) {
+        if (ele.addEventListener) {
+            ele.addEventListener(event, fn, false);
+        } else {
+            ele.attachEvent('on' + event, fn.bind(ele));
+        }
+    }
+
+    //兼容removeEventListener函数
+    function removeEventListener(ele, event, fn) {
+        if (ele.removeEventListener) {
+            ele.removeEventListener(event, fn, false);
+        } else {
+            ele.detachEvent('on' + event, fn.bind(ele));
+        }
+    }
     // 点击body收回下拉列表
     document.body.onclick = function (e) {
-        event.stopPropagation();
+        window.event ? window.event.cancelBubble = true : e.stopPropagation();
         document.getElementById('city-select').className = 'drop-select';
         document.getElementById('drop-down-select').children[1].className = "iconfont icon-arrow-down";
-    }
+    };
     // 城市选择
     document.getElementById('drop-down-select').onclick = function (event) {
-        event.stopPropagation();
+        window.event ? window.event.cancelBubble = true : e.stopPropagation();
         if (this.children[1].className === "iconfont icon-arrow-down") {
             this.children[1].className = "iconfont icon-arrow-up";
             document.getElementById('city-select').className = 'show drop-select';
-            for (let i = 0; i < 4; i++) {
+            for (var i = 0; i < 4; i++) {
                 document.getElementById('city-select').children[i].onclick = function (e) {
                     this.parentElement.parentElement.children[0].children[0].innerHTML = this.innerHTML;
-                    for (let i = 0; i < this.parentElement.childElementCount; i++) {
-                        this.parentElement.children[i].className = '';
+                    for (var _i = 0; _i < this.parentElement.childElementCount; _i++) {
+                        this.parentElement.children[_i].className = '';
                     }
                     this.className = 'active-selected';
-                }
+                };
             }
         } else {
             this.children[1].className = "iconfont icon-arrow-down";
             document.getElementById('city-select').className = 'drop-select';
         }
-    }
+    };
 
     // 搜索
     function areaSeltctShow(event) {
-        event.stopPropagation();
+        window.event ? window.event.cancelBubble = true : e.stopPropagation();
         if (this.parentElement.children[1].className === 'drop-select search-drop-select') {
             this.parentElement.children[1].className = 'drop-select search-drop-select show';
-            for (let i = 0; i < this.parentElement.children[1].childElementCount; i++) {
+            for (var i = 0; i < this.parentElement.children[1].childElementCount; i++) {
                 this.parentElement.children[1].children[i].onclick = function (e) {
                     // console.log(this.parentElement.parentElement.children[0]);
                     this.parentElement.parentElement.children[0].value = this.innerHTML;
-                }
+                };
             }
         } else {
             this.parentElement.children[1].className = 'drop-select search-drop-select';
@@ -44,65 +81,60 @@ window.onload = function (e) {
     document.getElementById('area-input').onmouseover = areaSeltctShow;
     document.getElementById('area-input').onmouseout = areaSeltctShow;
     document.getElementById('area-select').onmouseover = function (e) {
-        event.stopPropagation();
+        window.event ? window.event.cancelBubble = true : e.stopPropagation();
         this.parentElement.children[1].className = 'drop-select search-drop-select show';
-    }
+    };
     document.getElementById('area-select').onmouseout = function (e) {
-        event.stopPropagation();
+        window.event ? window.event.cancelBubble = true : e.stopPropagation();
         this.parentElement.children[1].className = 'drop-select search-drop-select';
-    }
+    };
 
     // 侧边栏
     document.getElementById('entrust').onmousemove = function (e) {
         window.event ? window.event.cancelBubble = true : e.stopPropagation();
         this.children[0].className = 'sidebar-show';
-    }
+    };
     document.getElementById('entrust').onmouseout = function (e) {
         window.event ? window.event.cancelBubble = true : e.stopPropagation();
         this.children[0].className = 'sidebar-show hide';
-
-    }
+    };
     document.getElementById('qr-code').onmouseover = function (e) {
         window.event ? window.event.cancelBubble = true : e.stopPropagation();
         this.children[0].className = 'sidebar-show';
-
-    }
+    };
     document.getElementById('qr-code').onmouseout = function (e) {
         window.event ? window.event.cancelBubble = true : e.stopPropagation();
         this.children[0].className = 'sidebar-show hide';
-
-    }
+    };
     // 滚动
     window.onscroll = function (e) {
-        let t = document.documentElement.scrollTop || document.body.scrollTop;
+        var t = document.documentElement.scrollTop || document.body.scrollTop;
         if (t > 668) {
             document.getElementById('search').className = 'search search-fixed';
         } else {
             document.getElementById('search').className = 'search';
-
         }
         if (t > 150) {
             document.getElementById('up-to-head').className = 'trans-hide slow-show iconfont icon-arrow-up';
-
         } else {
             document.getElementById('up-to-head').className = 'iconfont icon-arrow-up trans-hide';
         }
-    }
+    };
     document.getElementById('up-to-head').onmouseover = function (e) {
         if (document.getElementById('up-to-head').className === 'trans-hide slow-show iconfont icon-arrow-up')
             // console.log(document.getElementById('up-to-head').style.visibility);
             document.getElementById('up-top-show').className = 'sidebar-show';
-    }
+    };
     document.getElementById('up-to-head').onmouseout = function (e) {
         document.getElementById('up-top-show').className = 'sidebar-show hide';
-    }
+    };
 
     document.getElementById('up-to-head').onclick = function (e) {
         // document.documentElement.scrollTop=0;
-        let timer = null;
+        var timer = null;
         window.cancelAnimationFrame(timer);
         timer = window.requestAnimationFrame(function fn() {
-            let top = document.documentElement.scrollTop || document.documentElement.scrollTop;
+            var top = document.documentElement.scrollTop || document.documentElement.scrollTop;
             if (top > 0) {
                 document.body.scrollTop = document.documentElement.scrollTop = top - 100;
                 timer = window.requestAnimationFrame(fn);
@@ -111,30 +143,34 @@ window.onload = function (e) {
             }
         });
         document.getElementById('up-top-show').className = 'sidebar-show hide';
-    }
+    };
     // 品牌按钮
-    for (let i = 1; i < 4; i++) {
+
+    var _loop = function _loop(i) {
         document.getElementById('band-house-btns').children[i].onclick = function (e) {
             window.event ? window.event.cancelBubble = true : e.stopPropagation();
 
-            for (let j = 1; j < 4; j++) {
+            for (var j = 1; j < 4; j++) {
                 this.parentElement.children[j].className = 'band-house-btn';
             }
             this.className = 'band-house-btn active-btn';
-            for (let j = 1; j < 4; j++) {
-                this.parentElement.parentElement.children[j].className = 'band-house-show show-list display-none';
+            for (var _j = 1; _j < 4; _j++) {
+                this.parentElement.parentElement.children[_j].className = 'band-house-show show-list display-none';
             }
             this.parentElement.parentElement.children[i].className = 'band-house-show show-list display-show';
-            mySlide(`slide-show${i}`);
-        }
+            mySlide('slide-show' + i);
+        };
+    };
 
+    for (var i = 1; i < 4; i++) {
+        _loop(i);
     }
     //顶部轮播图
-    let imgs = document.getElementById('carousel-img').children;
-    let dots = document.getElementById('img-dot').children;
+    var imgs = document.getElementById('carousel-img').children;
+    var dots = document.getElementById('img-dot').children;
 
     document.getElementById('top-prev-btn').onclick = function (e) {
-        for (let i = 0; i < imgs.length; i++) {
+        for (var i = 0; i < imgs.length; i++) {
             if (imgs[i].className === 'display-show') {
                 if (i == 0) {
                     imgs[3].className = 'display-show';
@@ -151,9 +187,9 @@ window.onload = function (e) {
                 }
             }
         }
-    }
+    };
     document.getElementById('top-next-btn').onclick = function (e) {
-        for (let i = 0; i < imgs.length; i++) {
+        for (var i = 0; i < imgs.length; i++) {
             if (imgs[i].className === 'display-show') {
                 if (i == 3) {
                     imgs[0].className = 'display-show';
@@ -170,10 +206,10 @@ window.onload = function (e) {
                 }
             }
         }
-    }
+    };
     // 定时轮播图
-    let carouselTimer = setTimeout(function fn() {
-        for (let i = 0; i < imgs.length; i++) {
+    var carouselTimer = setTimeout(function fn() {
+        for (var i = 0; i < imgs.length; i++) {
             if (imgs[i].className === 'display-show') {
                 if (i == 3) {
                     imgs[0].className = 'display-show';
@@ -194,16 +230,21 @@ window.onload = function (e) {
         }
     }, 5000);
     // 头部轮播点
-    for (let i = 0; i < dots.length; i++) {
+
+    var _loop2 = function _loop2(i) {
         dots[i].onclick = function (e) {
-            for (let j = 0; j < 4; j++) {
+            for (var j = 0; j < 4; j++) {
                 imgs[j].className = 'display-none';
                 dots[j].className = 'iconfont';
             }
             window.event ? window.event.cancelBubble = true : e.stopPropagation();
             imgs[i].className = 'display-show';
             dots[i].className = 'iconfont active-dot';
-        }
+        };
+    };
+
+    for (var i = 0; i < dots.length; i++) {
+        _loop2(i);
     }
     //页面轮播图
     //动画函数
@@ -232,19 +273,19 @@ window.onload = function (e) {
 
     function mySlide(id) {
         //1. 找对象
-        let imgwidth = document.getElementById(id).offsetWidth;
+        var imgwidth = document.getElementById(id).offsetWidth;
         // console.log(imgwidth);
-        let slideshow = document.getElementById(id);
-        let box = slideshow.parentElement;
-        let ul = slideshow.children[0];
-        let ullis = ul.children;
-        let ol = slideshow.children[1];
-        let ollis;
-        let arr = slideshow.children[2];
-        let leftArr = arr.children[0];
-        let rightArr = arr.children[1];
-        let pic = 0;
-        let timer = null;
+        var slideshow = document.getElementById(id);
+        var box = slideshow.parentElement;
+        var ul = slideshow.children[0];
+        var ullis = ul.children;
+        var ol = slideshow.children[1];
+        var ollis = void 0;
+        var arr = slideshow.children[2];
+        var leftArr = arr.children[0];
+        var rightArr = arr.children[1];
+        var pic = 0;
+        var timer = null;
 
         function init() {
             for (var i = 0; i < ullis.length; i++) {
@@ -263,7 +304,7 @@ window.onload = function (e) {
             //3.1 给小方块注册点击事件
             for (var i = 0; i < ollis.length; i++) {
                 ollis[i].index = i; //存索引
-                ollis[i].addEventListener("click", function () {
+                addEventListener(ollis[i],"click", function () {
                     //3.2 小方块高亮排他
                     for (var i = 0; i < ollis.length; i++) {
                         ollis[i].className = "iconfont";
@@ -272,23 +313,23 @@ window.onload = function (e) {
                     //3.3. 移动ul
                     var target = -this.index * imgwidth;
                     animate(ul, target);
-                    pic = fk = this.index;
-                })
+                    pic = this.index;
+                });
             }
         };
         init();
         box.onmouseover = function () {
-            this.children[0].children[2].style.display = "block";
+            arr.style.display = "block";
             //清除定时器
             clearTimeout(timer);
-        }
+        };
         box.onmouseleave = function () {
             arr.style.display = "none";
             timer = setTimeout(function fn() {
                 rightArr.onclick();
                 timer = setTimeout(fn, 5000);
-            }, 5000)
-        }
+            }, 5000);
+        };
         //4.3 点击右箭头
         rightArr.onclick = function () {
             //如果已经到了最后一张假图片，让ul瞬移到第一张真图片
@@ -308,7 +349,7 @@ window.onload = function (e) {
             }
             var target = -pic * imgwidth;
             animate(ul, target);
-        }
+        };
         //4.4 点击左箭头
         leftArr.onclick = function () {
             if (pic === 0) {
@@ -320,14 +361,13 @@ window.onload = function (e) {
                 ollis[i].className = "iconfont";
             }
             if (pic === -1) {
-                ollis[ollis.length].className = 'iconfont active-dot'
+                ollis[ollis.length].className = 'iconfont active-dot';
             } else {
                 ollis[pic].className = "iconfont active-dot";
-
             }
             var target = -pic * imgwidth;
             animate(ul, target);
-        }
+        };
 
         timer = setTimeout(function fn() {
             rightArr.onclick();
@@ -336,4 +376,4 @@ window.onload = function (e) {
     }
     mySlide('slide-show1');
     mySlide('slide-show4');
-}
+};
